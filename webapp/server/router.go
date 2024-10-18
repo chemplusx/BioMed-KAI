8d9dfbfc0b7d48f2cafbb045d73bcbbaf974dc8a
Rohit <rohit.911.chauhan@gmail.com>
2024-10-18 08:04:04 +0530

    Initialise

---
+++ webapp/server/router.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"chemplusx.com/medal-ai/controller"
+	"github.com/gin-gonic/gin"
+	// "chemplusx.com/medal-ai/neo4j"
+)
+
+func InitRouter(r *gin.Engine) {
+
+	// Serve the OpenAPI specification and Swagger UI
+	r.StaticFile("/home", "./static/templates/home.html")
+	r.StaticFile("/medal-ai", "./static/templates/medal-ai.html")
+	r.StaticFile("/source", "./static/templates/sources.html")
+	r.StaticFile("/about", "./static/templates/about.html")
+	r.StaticFile("/contact", "./static/templates/contact.html")
+
+	r.Static("/static", "./static/assets")
+	r.Static("/js", "./static/js")
+	r.Static("/images", "./static/images")
+	r.Static("/css", "./static/css")
+
+	r.POST("/api/stream", controller.MedalAIHandler)
+
+	// Use the custom OpenAPI request validator middleware in Gin
+	// r.Use(middlewares.OapiRequestValidatorWithGin(router))
+	InitDataEndpoint(r)
+}
+
+/*
+InitDataEndpoint initializes the data endpoint for the API server.
+
+1: List node labels
+2: List Edge (Relationship) collection
+3: List label attributes
+4: Get Nodes by label
+	- limit
+5: Search nodes
+	- by search term, limit
+*/
+
+func InitDataEndpoint(r *gin.Engine) {
+	// client, err := neo4j.NewClient("bolt://localhost:7687", "neo4j", "password")
+	// if err != nil {
+	// 	log.Fatalf("Failed to create Neo4j client: %v", err)
+	// }
+
+	// r.GET("/ws", HandleWebSocket)
+
+	// // Define your API routes
+	// r.GET("/list/labels", controllers.GetLabelsHandler(client))
+	// r.GET("/list/relations", controllers.GetEdgesHandler(client))
+	// r.GET("/list/labels/:label", controllers.GetLabelDetailsHandler(client))
+	// r.POST("/nodes", controllers.GetNodesByRequestHandler(client))
+	// r.GET("/nodes/search", controllers.SearchNodesHandler(client))
+	// r.GET("/nodes/graph", controllers.GetNetworkGraphForIdHandler(client))
+	// r.GET("/search_in_graph", controllers.SearchNodesInGraphHandler(client))
+	// r.POST("/api/global-search", controllers.GlobalSearchHandler(client))
+	// r.POST("/api/interaction-search", controllers.InteractionSearchHandler(client))
+	// r.POST("/api/path-search", controllers.PathSearchHandler(client))
+
+}
---
+++ webapp/service/medal-ai-service.go
@@ -0,0 +1,147 @@
+package service
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// MedalAIRequest represents a request to the Medal AI service.
+type MedalAIRequest struct {
+	// The text to process
+	Text          string   `json:"text"`
+	Conversations []string `json:"conversations"`
+	Count         int      `json:"count"`
+}
+
+// Validate validates the request.
+func (r *MedalAIRequest) Validate() error {
+	if r.Text == "" {
+		return fmt.Errorf("text is required")
+	}
+	return nil
+}
+
+// MedalAIResponse represents a response from the Medal AI service.
+type MedalAIResponse struct {
+	// The text to process
+	Text string `json:"text"`
+}
+
+// readMultiLineInput reads input until an empty line is entered.
+func readMultiLineInput(reader *bufio.Reader) string {
+	var lines []string
+	fmt.Print(">>> ")
+
+	for {
+		line, err := reader.ReadString('\n')
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(0)
+			}
+			fmt.Printf("Reading the prompt failed: %s", err)
+			os.Exit(1)
+		}
+
+		if len(strings.TrimSpace(line)) == 0 {
+			break
+		}
+
+		lines = append(lines, line)
+	}
+
+	text := strings.Join(lines, "")
+	fmt.Println("Sending", text)
+	return text
+}
+
+var (
+	threads   = 32
+	tokens    = 4096
+	gpulayers = -1
+	seed      = -1
+)
+
+// ProcessMedalAIRequest processes a Medal AI request.
+func ProcessMedalAIRequest(req MedalAIRequest) (MedalAIResponse, error) {
+	// Process the request
+	// t := llama.LLama
+
+	// return MedalAIResponse{
+	// 	Text: strings.ToUpper(req.Text),
+	// }, nil
+
+	text := req.Text
+
+	callback := func(response string, bb bool) {
+		fmt.Println("Received response:", response)
+	}
+
+	Send("generate", text, callback)
+
+	input := `
+            <|begin_of_text|><|start_header_id|>system<|end_header_id|>
+            Environment: ipython
+            Tools: brave_search, wolfram_alpha
+            Cutting Knowledge Date: December 2023
+            Today Date: 25 Jul 2024
+
+            # Tool Instructions
+            - Always execute python code in messages that you share.
+            - When looking for real time information use relevant functions if available else fallback to brave_search
+
+
+
+            You have access to the following functions:
+
+            Use the function 'fetch_context' to: Get precise context on any medical entity you need more context on.
+            {
+                "name": "fetch_context",
+                "description": "Get top matching context for a given medical entity",
+                "parameters": {
+                    "text": {
+                        "param_type": "string",
+                        "description": "Medical Entity to get context on",
+                        "required": true
+                    },
+                    "label": {
+                        "param_type": "string",
+                        "description": "Probable Label of the entity (Choices: Drug, Disease, Gene, Protein, Metabolite, Pathway, Tissue, Compound)",
+                        "required": true
+                    }
+                }
+            }
+
+            If a you choose to call a function ONLY reply in the following format:
+            <{start_tag}={function_name}>{parameters}{end_tag}
+            where
+
+            start_tag => <function
+            parameters => a JSON dict with the function argument name as key and function argument value as value.
+            end_tag => </function>
+
+            Here is an example,
+            <function=example_function_name>{"example_name": "example_value"}</function>
+
+            Reminder:
+            - Function calls MUST follow the specified format
+            - Required parameters MUST be specified
+            - Only call one function at a time
+            - Put the entire function call reply on one line
+            - Always add your sources when using search results to answer the user query
+
+            You are a helpful assistant<|eot_id|>
+            <|start_header_id|>user<|end_header_id|>
+            Question: """ + $$prompt$$ + \
+            """<|eot_id|>
+            <|start_header_id|>assistant<|end_header_id|>
+        `
+	input = strings.Replace(input, "$$prompt$$", text, -1)
+
+	resp := MedalAIResponse{
+		Text: strings.ToUpper(req.Text),
+	}
+	return resp, nil
+}
